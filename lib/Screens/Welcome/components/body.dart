0d03d2101a868dad36d01b0053f22e4b72609c87
ITDhruv <120175094+ITDhruv@users.noreply.github.com>
2023-09-10 19:21:43 +0530

    ✨ Welcome Page

---
+++ lib/Screens/Welcome/components/body.dart
@@ -0,0 +1,46 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'package:flutter/material.dart';
+import 'package:signup_login/Screens/Welcome/components/background.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:signup_login/components/rounded_button.dart';
+import 'package:signup_login/constants.dart';
+// import 'package:signup_login/constants.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              "Welcome!",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.03,),
+            SvgPicture.asset(
+              "assets/icons/chat.svg",
+              height: size.height * 0.45,
+            ),
+            SizedBox(height: size.height * 0.05,),
+            RoundedButton(
+              text: "LOGIN",
+              press: () {},
+            ),
+            RoundedButton(
+              text: "LOGIN",
+              color: kPrimaryLightColor,
+              textColor: Colors.black,
+              press: () {},
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/components/rounded_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:signup_login/constants.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+  final void Function() press;
+  final Color color, textColor;
+  const RoundedButton({
+    super.key,
+    required this.text,
+    required this.press,
+    this.color = kPrimaryColor,
+    this.textColor = Colors.white,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      width: size.width * 0.8,
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(29),
+        child: FloatingActionButton(
+          backgroundColor: color,
+          onPressed: press,
+          child: Padding(
+            padding: EdgeInsets.symmetric(
+              vertical: 20,
+              horizontal: 40,
+            ),
+            child: Text(
+              text,
+              style: TextStyle(color: textColor),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
